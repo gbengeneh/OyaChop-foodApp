df967beed63e72cf518f9a95cc8195572e179f3f
unknown <j.telher@gmail.com>
2023-05-16 21:14:12 +0100

    first commit

---
+++ public/js/landing.js
@@ -0,0 +1,12 @@
+const menuIcon = document.querySelector(".menu-icon")
+    const menuList = document.querySelector(".menu-list")
+    
+    menuIcon.addEventListener("click", () =>{
+      menuIcon.classList.toggle("active")
+      menuList.classList.toggle("active")
+    })
+
+    document.querySelectorAll(".link").forEach(n => n.addEventListener("click",() => {
+      menuIcon.classList.remove("active")
+      menuList.classList.remove("active")
+    }))
