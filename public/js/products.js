3465777d7b20582b7ed68f8026d46c96faa9bfca
unknown <j.telher@gmail.com>
2023-05-23 11:07:47 +0100

    second commit

--- public/js/products.js
+++ public/js/products.js
@@ -1,10 +1,84 @@
 const products = [
   {
     id: 0,
-    name: "Eforiro with Eba",
+    name: "Eba with Efo riro",
     price: 1500.00,
     decription: "delicious and yummy",
-    imgSrc: "..\img\foodItems\efo riro with garri.jpg",
+    imgSrc: ".public\img\foodItems\efo riro with garri.jpg",
   },
+
+  {
+    id: 1,
+    name: "Amala with Ewedu Soup",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\amala and ewedu.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Cheese with Pupkin Soup",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\cheese and pupkin soup.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Eba with Edikang Ikong Soup",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\edikang ikong soup.jpg",
+  },
+
+  {
+    id: 4,
+    name: "Semo with Eforiro and Okro Soup",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\efo riro with okro soup.jpg",
+  },
+
+  {
+    id: 5,
+    name: "Pounded Yam with Egusi Soup",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\egusi soup with pounded yam.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Fried Rice with Chicken",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\friedrice with sauce.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Amala and Ewedu",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\amala and ewedu.jpg",
+  },
+
+  {
+    id: 8,
+    name: "Amala and Ewedu",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\amala and ewedu.jpg",
+  },
+
+  {
+    id: 9,
+    name: "Amala and Ewedu",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: ".public\img\foodItems\amala and ewedu.jpg",
+  },
+
+  
 
 ]
