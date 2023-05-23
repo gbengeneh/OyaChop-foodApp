69e17d6261354d7f6854e0861f349f641d4f3e3d
unknown <j.telher@gmail.com>
2023-05-23 10:56:59 +0100

    second commit

---
+++ public/js/products.js
@@ -0,0 +1,10 @@
+const products = [
+  {
+    id: 0,
+    name: "Eforiro with Eba",
+    price: 1500.00,
+    decription: "delicious and yummy",
+    imgSrc: "..\img\foodItems\efo riro with garri.jpg",
+  },
+
+]
