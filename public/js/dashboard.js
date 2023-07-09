e22f51110ebed38603124df8feea780be04a310a
unknown <j.telher@gmail.com>
2023-07-09 04:46:56 +0100

    Last Update as at July 9th, 2023

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,6 +4,7 @@ const sideBar = document.querySelector(".sidebar-container")
 menuIcon.addEventListener("click", () =>{
 menuIcon.classList.toggle("active")
 sideBar.classList.toggle("active")
+
 })
 
 document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click",() => {
@@ -11,3 +12,25 @@ menuIcon.classList.remove("active")
 sideBar.classList.remove("active")
 }))
 
+
+
+// Handle order status selection
+
+const orderStatus = document.querySelectorAll(".orderStatus");
+  
+
+function handleOrderStatus(event) {
+  const selectedOrderBtn = event.target;
+  if (selectedOrderBtn.classList.contains("orderStatus")) {
+    const activeOrderToggle = document.querySelector(".orderStatus.active");
+    if (activeOrderToggle) {
+      activeOrderToggle.classList.remove("active");
+    }
+    selectedOrderBtn.classList.add("active");
+  }
+}
+
+// Add event listener to orderStatus elements
+orderStatus.forEach((button) => {
+  button.addEventListener("click", handleOrderStatus);
+});
